679e19ee52c7c2ad237358401265bb31cdf01456
Jia <jia9943657@gmail.com>
2025-08-28 16:49:57 +0800

    implement mainActivity, bottom_nav and nav_graph

---
+++ app/src/main/java/com/team/wordsapp_casestudy/ui/manage/detail/DetailFragment.kt
@@ -0,0 +1,19 @@
+package com.team.wordsapp_casestudy.ui.manage.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.team.wordsapp_casestudy.R
+
+class DetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+}
