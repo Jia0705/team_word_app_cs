5727a99384d08ba6e262576cb31dd952fa23b7d4
Jay <jayyongshengloh@gmail.com>
2025-09-02 15:26:02 +0800

    Added Base Manage Fragment and ViewModel

--- app/src/main/java/com/team/wordsapp_casestudy/ui/manage/base/BaseManageWordFragment.kt
+++ app/src/main/java/com/team/wordsapp_casestudy/ui/manage/base/BaseManageWordFragment.kt
@@ -1,21 +1,41 @@
 package com.team.wordsapp_casestudy.ui.manage.base
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.team.wordsapp_casestudy.R
+import androidx.fragment.app.setFragmentResult
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.team.wordsapp_casestudy.databinding.FragmentBaseManageWordBinding
+import kotlinx.coroutines.launch
 
-class BaseManageWordFragment : Fragment() {
+abstract class BaseManageWordFragment : Fragment() {
 
-    private val viewModel: BaseManageWordViewModel by viewModels()
+    protected abstract val viewModel: BaseManageWordViewModel
+
+    protected lateinit var binding: FragmentBaseManageWordBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_base_manage_word, container, false)
+        binding = FragmentBaseManageWordBinding.inflate(
+            layoutInflater,
+            container,
+            false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launch {
+            viewModel.finish.collect {
+                setFragmentResult("manage_word", Bundle())
+                findNavController().popBackStack()
+            }
+        }
     }
 }
