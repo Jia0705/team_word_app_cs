cee6000f4a03e6a120a4507b248fd10f40c94aa5
Jia <jia9943657@gmail.com>
2025-09-02 15:36:21 +0800

    implement HomeFragment, HomeViewModel, xml and adapter (Strings and colors too)

--- app/src/main/java/com/team/wordsapp_casestudy/ui/home/HomeFragment.kt
+++ app/src/main/java/com/team/wordsapp_casestudy/ui/home/HomeFragment.kt
@@ -5,14 +5,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.team.wordsapp_casestudy.R
+import androidx.fragment.app.setFragmentResultListener
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.team.wordsapp_casestudy.databinding.FragmentHomeBinding
+import com.team.wordsapp_casestudy.ui.adapter.WordsAdapter
+import kotlinx.coroutines.launch
+import kotlin.getValue
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    private val viewModel: HomeViewModel by viewModels()
+    private lateinit var adapter: WordsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupAdapter()
+
+        lifecycleScope.launch {
+            viewModel.words.collect {
+                adapter.setWords(it)
+                binding.llEmpty.visibility = if (it.isEmpty()) View.VISIBLE else View.GONE
+            }
+        }
+
+        // Add
+        binding.fabAdd.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToAddWordFragment()
+            findNavController().navigate(action)
+        }
+
+        setFragmentResultListener("manage_word") { _, _ ->
+            viewModel.getWords()
+        }
+    }
+
+    fun setupAdapter() {
+        adapter = WordsAdapter(emptyList()) {
+            val action = HomeFragmentDirections.actionHomeFragmentToDetailFragment(it.id!!)
+            findNavController().navigate(action)
+        }
+        binding.rvWords.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvWords.adapter = adapter
     }
 }
