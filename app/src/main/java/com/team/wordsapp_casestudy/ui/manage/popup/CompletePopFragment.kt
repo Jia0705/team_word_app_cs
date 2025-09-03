274178dac1c19ac48f2c939f5dea53f68d32d08c
Jay <jayyongshengloh@gmail.com>
2025-09-03 09:11:09 +0800

    Added Complete and Delete Popup fragment and xml. updated: dimens (tx_xxxxl: 30sp) and strings (are you sure, delete this word, move this word, cancel, yes and no )

--- app/src/main/java/com/team/wordsapp_casestudy/ui/manage/popup/CompletePopFragment.kt
+++ app/src/main/java/com/team/wordsapp_casestudy/ui/manage/popup/CompletePopFragment.kt
@@ -1,18 +1,56 @@
 package com.team.wordsapp_casestudy.ui.manage.popup
 
+import android.app.Dialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.button.MaterialButton
 import com.team.wordsapp_casestudy.R
-class CompletePopFragment : Fragment() {
+class CompletePopFragment : DialogFragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    interface Listener {
 
-        return inflater.inflate(R.layout.fragment_complete_pop, container, false)
+        fun onClickYes()
+
+        fun onClickNo()
+    }
+
+    private var listener: Listener? = null
+
+    fun setListener(listener: Listener) {
+        this.listener = listener
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        val view = LayoutInflater.from(requireContext()).inflate(
+            R.layout.fragment_complete_pop,
+            null
+        )
+
+        val btnYes = view.findViewById<MaterialButton>(R.id.mbYes)
+        val btnNo = view.findViewById<MaterialButton>(R.id.mbNo)
+
+        val dialog = AlertDialog.Builder(requireContext())
+            .setView(view)
+            .create()
+
+        btnYes.setOnClickListener {
+            listener?.onClickYes()
+            dialog.dismiss()
+        }
+
+        btnNo.setOnClickListener {
+            listener?.onClickNo()
+            dialog.dismiss()
+        }
+
+        return dialog
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        listener = null
     }
 }
--- app/src/main/java/com/team/wordsapp_casestudy/ui/manage/popup/DeletePopFragment.kt
+++ app/src/main/java/com/team/wordsapp_casestudy/ui/manage/popup/DeletePopFragment.kt
@@ -1,19 +1,60 @@
 package com.team.wordsapp_casestudy.ui.manage.popup
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.button.MaterialButton
 import com.team.wordsapp_casestudy.R
 
-class DeletePopFragment : Fragment() {
+class DeletePopFragment : DialogFragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_delete_pop, container, false)
+    interface Listener {
+        fun onClickConfirm()
+
+        fun onClickDelete()
+    }
+
+    private var listener: Listener? = null
+
+    fun setListener(listener: Listener) {
+        this.listener = listener
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        val view = LayoutInflater.from(requireContext())
+            .inflate(
+                R.layout.fragment_delete_pop,
+                null
+            )
+
+        val btnCancel = view.findViewById<MaterialButton>(R.id.mbCancel)
+        val btnDelete = view.findViewById<MaterialButton>(R.id.mbDelete)
+
+        val dialog = AlertDialog.Builder(requireContext())
+            .setView(view)
+            .create()
+
+        btnCancel.setOnClickListener {
+            listener?.onClickConfirm()
+            dialog.dismiss()
+        }
+
+        btnDelete.setOnClickListener {
+            listener?.onClickDelete()
+            dialog.dismiss()
+        }
+
+        return dialog
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        listener = null
     }
 }
